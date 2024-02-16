28df8da6c6af65f0887dbe5a747677dc39d632e5
axelsolea <160124333+axelsolea@users.noreply.github.com>
2024-02-16 08:59:50 +0100

    script fail2ban terminé

--- fail2ban.sh
+++ fail2ban.sh
@@ -1,2 +1,15 @@
 #!/bin/env bash
-sshpass -p motdepasseerrone ssh admin@172.18.0.4
+#Author : Axel 
+#Date : 15/02/2024
+#Last edit : 16/02/2024
+#Desc : make an ip get banned via ssh on asterisk
+#ip=192.168.141.252
+ip=172.18.0.4
+
+echo "Lancement du script..."
+for i in 1 2 3 4
+do
+	echo "Tentative $i" 
+	sshpass -p motdepasseerrone ssh admin-solea@${ip}
+done
+

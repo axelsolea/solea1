ee73319d1463a1aa03e7841faa7bbce8b3c5f983
axelsolea <160124333+axelsolea@users.noreply.github.com>
2024-02-16 16:54:40 +0100

    Update rawdns.sh
    correction de bugs

--- rawdns.sh
+++ rawdns.sh
@@ -7,29 +7,29 @@
 echo "Script started"
 
 echo "Resolving resolver1"
-nslookup q=A resolver1.solea.local
+nslookup resolver1.solea.local
 nslookup 172.18.0.200
 
 echo "Resolving zone1"
-nslookup q=A zone1.solea.local
+nslookup zone1.solea.local
 nslookup 172.18.0.202
 
 echo "Resolving dhcp"
-nslookup q=A dhcp.solea.local
+nslookup dhcp.solea.local
 nslookup 172.18.0.253
 
 echo "Resolving toip"
-nslookup q=A toip.solea.local
+nslookup toip.solea.local
 nslookup 172.18.0.4
 
 echo "Resolving share (samba)"
-nslookup q=A share.solea.local
+nslookup share.solea.local
 nslookup 172.18.0.252
 
 echo "Resolving gw"
-nslookup q=A gw.solea.local
+nslookup gw.solea.local
 nslookup 172.18.0.254
 
 echo "Resolving esxi"
-nslookup q=A esxi.solea.local
-nslookup 192.168.141.3
+nslookup esxi.solea.local
+#nslookup 192.168.141.3 => entrée ptr inexistante

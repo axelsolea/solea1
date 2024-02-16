b5b3845a87f247397ed40ac4d9ec974f46b2c42c
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-02-16 10:18:28 +0100

    Add files via upload

---
+++ test_dhcp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author: Djetic Alexandre
+# Date: 16/02/2023
+# Modified: 16/02/2023
+# Description: This script test the DHCP server
+
+if [ ! $EUID -eq 0 ]; then
+	echo "require sudo/root access"
+	exit 1
+fi
+
+dhclient -r >> dhcp_client.log
+dhclient -i ens160 >> dhcp_client.log

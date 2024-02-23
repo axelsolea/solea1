33b52e5878308ecb3804a9ec067bcbc2e7622c78
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-02-23 17:07:34 +0100

    Update test_dns.py

--- test_dns.py
+++ test_dns.py
@@ -34,6 +34,22 @@ def get_a_record_name(name: str, server: str) -> list:
     output: str = result.stdout
     return re.findall(pattern_a_record, output)
 
+def get_aaaa_record_name(name: str, server: str) -> list:
+    """
+    Get the AAAA record name from a DNS server.
+
+    Args:
+        name (str): The name to query.
+        server (str): The DNS server to query.
+
+    Returns:
+        list: List of IPv6 addresses.
+    """
+    pattern_aaaa_record: str = r'Address: ([a-fA-F0-9:]+)'
+    result = subprocess.run(["nslookup", "-query=AAAA", name, server], capture_output=True, text=True)
+    output: str = result.stdout
+    return re.findall(pattern_aaaa_record, output)
+
 def get_ptr_record_name(ip: str, server: str) -> list:
     """
     Get the PTR record name from an IP address.
@@ -45,12 +61,18 @@ def get_ptr_record_name(ip: str, server: str) -> list:
     Returns:
         list: List of PTR records.
     """
+
+    if ":" not in ip:
+        pattern_ptr_record: str = r'(?<=name = )\S+' #ipv4
+    else:
+        pattern_ptr_record: str = r'name = (\S+)' #ipv6
+
     pattern_ptr_record: str = r'(?<=name = )\S+'
     result = subprocess.run(["nslookup", "-q=PTR", ip, server], capture_output=True, text=True)
     output: str = result.stdout
     return re.findall(pattern_ptr_record, output)
 
-def compare_a_ptr_record(name: str, ptr_list_name: list) -> bool:
+def compare_record(name: str, ptr_list_name: list) -> bool:
     """
     Compare a name to a list of PTR records.
 
@@ -66,9 +88,77 @@ def compare_a_ptr_record(name: str, ptr_list_name: list) -> bool:
             return False
     return True
 
+def test_ipv4_name(name: str, server: str, log: Logger) -> bool:
+    """
+    Test IPv4 DNS name resolution by checking its A and PTR records.
+
+    Args:
+        name (str): The IPv4 DNS name to test.
+        server (str): The DNS server to query.
+        log (Logger): Logger object for logging results.
+
+    Returns:
+        bool: True if the test passes, False otherwise.
+    """
+    # A record
+    ip_addr: list = get_a_record_name(name, server)
+    ip_addr_str: str = ", ".join(ip_addr)  # Cast to str(log)
+
+    if not ip_addr_str: 
+        log.writeFile("ERROR", f"no A record found for {name}")
+        return False
+    else:
+        log.writeFile("NOTICE", f"A [{ip_addr_str}]")
+
+    # Check PTR record for A record
+    for ip in ip_addr:
+        list_name: list = get_ptr_record_name(ip, server)
+        ptr_name_str: str = ", ".join(list_name)
+        log.writeFile("NOTICE", f"Resolving inverse query(v4) {ip}")
+        log.writeFile("NOTICE", f"PTR[{ptr_name_str[0:len(ptr_name_str) - 1]}]")
+        
+        if not compare_record(name, list_name):
+            return False
+
+    return True
+
+
+def test_ipv6_name(name: str, server: str, log: Logger) -> bool:
+    """
+    Test IPv6 DNS name resolution by checking its AAAA and PTR records.
+
+    Args:
+        name (str): The IPv6 DNS name to test.
+        server (str): The DNS server to query.
+        log (Logger): Logger object for logging results.
+
+    Returns:
+        bool: True if the test passes, False otherwise.
+    """
+    # AAAA records
+    ipv6_addr: list = get_aaaa_record_name(name, server)
+    ipv6_addr_str: str = ", ".join(ipv6_addr)
+
+    if not ipv6_addr_str: 
+        log.writeFile("ERROR", f"no AAAA record found for {name}")
+    else:
+        log.writeFile("NOTICE", f"AAAA [{ipv6_addr_str}]")
+    
+    # Check PTR records for AAAA records
+    for ipv6 in ipv6_addr:
+        list_name: list = get_ptr_record_name(ipv6, server)
+        ptr_name_str: str = ", ".join(list_name)
+        log.writeFile("NOTICE", f"Resolving inverse query(v6) {ipv6}")
+        log.writeFile("NOTICE", f"PTR[{ptr_name_str[0:len(ptr_name_str) - 1]}]")
+        
+        if not compare_record(name, list_name):
+            return False
+
+    return True
+
 def test_name(name: str, server: str, log: Logger) -> bool:
     """
-    Test a DNS name by checking its A and PTR records.
+    Test a DNS name by checking its A, AAAA and PTR records.
 
     Args:
         name (str): The DNS name to test.
@@ -78,27 +168,9 @@ def test_name(name: str, server: str, log: Logger) -> bool:
     Returns:
         bool: True if the test passes, False otherwise.
     """
-    ip_addr: list = get_a_record_name(name, server)
-    ip_addr_str: str = ", ".join(ip_addr)
+    return test_ipv4_name(name, server, log) and test_ipv6_name(name, server, log)
 
-    if not ip_addr_str: 
-        log.writeFile("ERROR", f"no record found for {name}")
-        return False
-    else:
-        log.writeFile("NOTICE", f"A [{ip_addr_str}]")
-
-    for ip in ip_addr:
-        list_name: list = get_ptr_record_name(ip, server)
-        ptr_name_str: str = ", ".join(list_name)
-        log.writeFile("NOTICE", f"Resolving inverse query {ip}")
-        log.writeFile("NOTICE", f"PTR[{ptr_name_str[0:len(ptr_name_str) - 1]}]")
-        
-        if not compare_a_ptr_record(name, list_name):
-            return False
-
-    return True
-        
-if __name__ == "__main__":
+def main() -> None:
     current_dir: str = os.getcwd()
     nb_name: int = 0
     nb_success_request: int = 0
@@ -124,6 +196,9 @@ if __name__ == "__main__":
                 log.writeFile("INFO", f"{name} => NOK\n\n")
 
     if nb_name == nb_success_request:
-        sys.exit(0)
+        print("0")
     else:
-        sys.exit(1)
+        print("1")
+
+if __name__ == "__main__":
+    main()

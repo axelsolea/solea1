289e55410a01f46a84c39ad0b7a40a7114ba35bf
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-02-23 11:14:44 +0100

    Update test_dns.py

--- test_dns.py
+++ test_dns.py
@@ -17,7 +17,7 @@ def test_name(name: str, server: str, log: Logger) -> None:
     result = subprocess.run(["nslookup", name, server], capture_output=True, text=True)
     output: str = result.stdout
     ip_addr_str: str = ", ".join(re.findall(pattern_a_record, output))
-    log.writeFile("NOTICE", f"found for {name} : {ip_addr_str}")
+    log.writeFile("NOTICE", f"A [{ip_addr_str}]")
 
     #check A record that is not empty
     if not re.search(pattern_a_record, output):
@@ -29,7 +29,8 @@ def test_name(name: str, server: str, log: Logger) -> None:
         output: str = result.stdout
         list_name: list = re.findall(pattern_ptr_record, output)
         ptr_name_str: str = ", ".join(re.findall(pattern_ptr_record, output))
-        log.writeFile("NOTICE", f"found for {ip} : {ptr_name_str}")
+        log.writeFile("NOTICE", f"Resolving inverse query {ip}")
+        log.writeFile("NOTICE", f"PTR[{ptr_name_str}]")
 
         for ptr_name in list_name:
             if ptr_name != name + ".":
@@ -56,21 +57,21 @@ if __name__ == "__main__":
     #add all name to logfile
     for name in data:
         if name and "#" not in name: # '#' est un début commentaire
-            log.writeFile("INFO", f"adding to list {name}")
+            log.writeFile("INFO", f"adding to testing list {name}")
             nb_name += 1
-
+    
     #send query
     for name in data:
         if name and not name.startswith("#"):
+            log.writeFile("INFO", f"Resolving {name}")
             if test_name(name, dns_server, log):
-                log.writeFile("INFO", f"{name} OK")
+                log.writeFile("INFO", f"{name} => OK\n\n")
                 nb_success_request += 1
             else:
-                log.writeFile("INFO", f"{name} NOK")
+                log.writeFile("INFO", f"{name} => NOK\n\n")
 
     #check the number of name is the same as the number of succes request 
     if nb_name == nb_success_request:
         print("0")
     else:
         print("1")
-

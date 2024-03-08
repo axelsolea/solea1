48e317c953844c5d705a6b39816d3d9ead40daf6
axelsolea <160124333+axelsolea@users.noreply.github.com>
2024-03-08 18:49:57 +0100

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -3,8 +3,8 @@
 #Date : 15/02/2024
 #Last edit : 16/02/2024
 #Desc : make an ip get banned via ssh on asterisk
-#ip=192.168.141.252
-ip=172.18.0.4
+ip=192.168.141.252
+#ip=172.18.0.4
 
 echo "Lancement du script..."
 for i in 1 2 3 4

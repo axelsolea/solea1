23246103c017497988f764b9ce676c00f8287952
axelsolea <160124333+axelsolea@users.noreply.github.com>
2024-03-08 18:10:42 +0100

    Update test_nat_pat_v2.sh

--- test_nat_pat_v2.sh
+++ test_nat_pat_v2.sh
@@ -8,22 +8,25 @@ echo "Lancement du script..."
 PORTS="22202 22253"
 SCRIPT="echo 'Nom de la machine : '; hostname"
 for PORT in ${PORTS} ; do
-    echo "Accès SSH via l'ip publique sur le port ${PORT}"  
+    echo "Accès SSH via l'ip publique (192.168.141.2) sur le port ${PORT}"  
     sshpass -p Solea05axel ssh axel@192.168.141.2 -p ${PORT} "${SCRIPT}"
     echo ""
 done
 
-echo "Accès SSH via l'ip publique sur le port 22200"  
+echo "Accès SSH via l'ip publique (192.168.141.2) sur le port 22200"  
 echo 'Nom de la machine : '
 sshpass -p Solea05dns ssh admin-solea@192.168.141.2 -p 22200 "hostname"
 echo ""
 
-echo "Accès SSH via l'ip publique sur le port 22252"  
+echo "Accès SSH via l'ip publique (192.168.141.2) sur le port 22252"  
 echo 'Nom de la machine : '
 sshpass -p " " ssh admin-solea@192.168.141.2 -p 22252 "hostname"
+echo ""
 
-echo "Accès SSH via l'ip publique sur le port 22004"  
+echo "Accès SSH via l'ip publique (192.168.141.2) sur le port 22004"  
 echo "ignoré tant que le serveur toip.solea.local a une adresse sur le VLAN 140"
 #echo 'Nom de la machine : '
 #sshpass -p " " ssh admin-solea@192.168.141.2 -p 22004 "hostname"
+echo ""
+
 echo "Arrêt du script..."

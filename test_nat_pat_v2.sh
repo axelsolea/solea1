21dfe85514b1bafdd2ca80ec6e67249105f620da
axelsolea <160124333+axelsolea@users.noreply.github.com>
2024-03-08 17:44:15 +0100

    Update test_nat_pat_v2.sh

--- test_nat_pat_v2.sh
+++ test_nat_pat_v2.sh
@@ -3,17 +3,8 @@
 #Creation date : 08/03/2024
 #Last edit : 08/03/2024
 
-sshpass -p Solea05asterisk ssh admin-solea@192.168.141.2 -p 22004
-hostname; exit
-
-sshpass -p Solea05axel ssh axel@192.168.141.2 -p 22200
-hostname; exit
-
-sshpass -p Solea05axel ssh axel@192.168.141.2 -p 22202
-hostname; exit
-
-sshpass -p Solea05axel ssh axel@192.168.141.2 -p 22253
-hostname; exit
-
-sshpass -p Solea05axel ssh axel@192.168.141.2 -p 22252
-hostname; exit
+PORTS="22200 22202 22253 22252 22004"
+SCRIPT="hostname"
+for PORT in ${PORTS} ; do
+    sshpass -p Solea05axel ssh axel@192.168.141.2 -p ${PORT} "${SCRIPT}"
+done

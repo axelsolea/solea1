5ce71e50ba9c35a575f3fec6932bb49f9a1ed5d8
axelsolea <160124333+axelsolea@users.noreply.github.com>
2024-03-08 18:04:47 +0100

    Update test_nat_pat_v2.sh

--- test_nat_pat_v2.sh
+++ test_nat_pat_v2.sh
@@ -3,8 +3,8 @@
 #Creation date : 08/03/2024
 #Last edit : 08/03/2024
 
-PORTS="22202 22253 22252" #22004
-SCRIPT="echo "Nom de la machine : "; hostname"
+PORTS="22202 22253"
+SCRIPT="echo 'Nom de la machine : '; hostname"
 for PORT in ${PORTS} ; do
     echo "Accès SSH via l'ip publique sur le port ${PORT}"  
     sshpass -p Solea05axel ssh axel@192.168.141.2 -p ${PORT} "${SCRIPT}"
@@ -13,3 +13,6 @@ done
 
 echo "Accès SSH via l'ip publique sur le port 22200"  
 sshpass -p Solea05dns ssh admin-solea@192.168.141.2 -p 22200 "hostname"
+
+echo "Accès SSH via l'ip publique sur le port 22252"  
+sshpass -p   ssh admin-solea@192.168.141.2 -p 22252 "hostname"

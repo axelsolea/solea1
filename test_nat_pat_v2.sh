c621ff2d7aae32f65b5ab8380d111bd84c9f4f8d
axelsolea <160124333+axelsolea@users.noreply.github.com>
2024-03-08 18:08:38 +0100

    Update test_nat_pat_v2.sh

--- test_nat_pat_v2.sh
+++ test_nat_pat_v2.sh
@@ -19,3 +19,8 @@ echo ""
 echo "Accès SSH via l'ip publique sur le port 22252"  
 echo 'Nom de la machine : '
 sshpass -p " " ssh admin-solea@192.168.141.2 -p 22252 "hostname"
+
+echo "Accès SSH via l'ip publique sur le port 22004"  
+echo "ignoré tant que le serveur toip.solea.local a une adresse sur le VLAN 140"
+#echo 'Nom de la machine : '
+#sshpass -p " " ssh admin-solea@192.168.141.2 -p 22004 "hostname"

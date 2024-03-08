4adb94674f0e7b4407e71666bdcf7b77f6bf8705
axelsolea <160124333+axelsolea@users.noreply.github.com>
2024-03-08 17:36:59 +0100

    Create test_nat_pat_v2.sh

---
+++ test_nat_pat_v2.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+#Author : Axel CHALVIN
+#Creation date : 08/03/2024
+#Last edit : 08/03/2024
+
+sshpass -p Solea05asterisk ssh admin-solea@192.168.141.2 -p 22004
+hostname; exit
+
+sshpass -p Solea05axel ssh axel@192.168.141.2 -p 22200
+hostname; exit
+
+sshpass -p Solea05axel ssh axel@192.168.141.2 -p 22202
+hostname; exit
+
+sshpass -p Solea05axel ssh axel@192.168.141.2 -p 22253
+hostname; exit
+
+sshpass -p Solea05axel ssh axel@192.168.141.2 -p 22252
+hostname; exit

6fa8b12a12bb23469e1a0a466d69c37f73aa5990
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-04-05 15:22:27 +0200

    Update test_ntp.sh

--- test_ntp.sh
+++ test_ntp.sh
@@ -1,34 +1,34 @@
-#!/bin/sh
-# Author: Djetic Alexandre
+#!/bin/bash
+# Auteur: Alexandre Djetic
 # Date: 05/04/2024
-# Modified: 05/04/2024
-# Description: This script test that all server is sync NTP
+# Modifié: 05/04/2024
+# Description: Ce script vérifie que tous les serveurs sont synchronisés via NTP
 
 ##############################################################################################
-# Ce script va acceder en ssh entre chaque serveur et vérifier que le serveur est synchronisé
-# Il obtient le status, date et heure du serveur
+# Ce script se connecte en ssh à chaque serveur et vérifie s'il est synchronisé
+# Il obtient le statut, la date et l'heure du serveur
 ##############################################################################################
 
-# couleur
-RED="\e[31m"
-NOCOLOR="\e[0m"
-GREEN="\033[0;32m"
+# Couleurs
+ROUGE="\e[31m"
+SANS_COULEUR="\e[0m"
+VERT="\033[0;32m"
 
-# env var
-FILE_SERVER="$1"
+# Variables d'environnement
+FICHIER_SERVEUR="$1"
 
-for server in $(< "$FILE_SERVER")
+for serveur in $(cat "$FICHIER_SERVEUR")
 do
-  timectl_return=$(ssh axel@${server} "timedatectl")
-  active_state=$(${timectl_return} | grep "NTP" | awk '{print $3}')
-  current_date=$(${timectl_return} | grep "Local time" | awk '{print $4}')
-  hour=$(${timectl_return} | grep "Local time" | awk '{print $5}')
+  resultat_timectl=$(ssh axel@$serveur "timedatectl")
+  etat_actif=$(echo "$resultat_timectl" | grep "NTP" | awk '{print $3}')
+  date_actuelle=$(echo "$resultat_timectl" | grep "Local time" | awk '{print $4}')
+  heure=$(echo "$resultat_timectl" | grep "Local time" | awk '{print $5}')
   
-  # affichage des informations
+  # Affichage des informations
   echo "---------------------------------------------"
-  echo -e "nom du server: ${RED}$server${NOCOLOR}"
-  echo -e "status: ${GREEN}$active_state${NOCOLOR}"
-  echo "date: $current_date"
-  echo -e "heure: ${GREEN}$hour${NOCOLOR}"
+  echo -e "Nom du serveur: ${ROUGE}$serveur${SANS_COULEUR}"
+  echo -e "Statut: ${VERT}$etat_actif${SANS_COULEUR}"
+  echo "Date: $date_actuelle"
+  echo -e "Heure: ${VERT}$heure${SANS_COULEUR}"
   echo -e "---------------------------------------------\n"
 done

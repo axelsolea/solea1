9de64c8788555716eb6ad013d2968de992a55f1f
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-04-05 16:28:25 +0200

    Update test_internet_v2.sh

--- test_internet_v2.sh
+++ test_internet_v2.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
-#Author : Axel CHALVIN
-#Creation date : 08/03/2024
-#Last edit : 05/04/2024
+# Auteur : Axel CHALVIN
+# Date de création : 08/03/2024
+# Dernière modification : 05/04/2024
 
 function test_ping {
-  # $1 : name or IP
-  value=$(ping -c 1 "$1" | grep "ping statistics" -A 1) 
-  echo "$value" >> test_internet_v2.log
-  return_value=$(echo "$value" | awk 'NR==2 {print $6}')
+  # $1 : nom ou IP
 
-  echo "nom: $1"
-  if [[ "$return_value" == "0%" ]];then
-    echo -e"connexion: \nOK ✓"
+  # couleurs
+  ROUGE="\e[31m"
+  AUCUNE_COULEUR="\e[0m"
+  VERT="\033[0;32m"
+
+  # récupération des valeurs
+  valeur=$(ping -c 1 "$1" | grep "ping statistics" -A 1) 
+  echo "$valeur" >> test_internet_v2.log
+  valeur_retour=$(echo "$valeur" | awk 'NR==2 {print $6}')
+  
+  echo -e "Nom : ${ROUGE}${1}${AUCUNE_COULEUR}"
+  if [[ "$valeur_retour" == "0%" ]];then
+    echo -e "- Connexion : ${VERT}OK ✓${AUCUNE_COULEUR}"
   else
-    echo -e "connexion: \nNOK ✘" 
+    echo -e "- Connexion : ${ROUGE}NOK ✘${AUCUNE_COULEUR}" 
   fi
 }
 
-echo "Script lancé"
-echo "Script started : $(date)" > test_internet_v2.log
+echo "Lancement du script"
+echo "Début du script : $(date)" > test_internet_v2.log
 
 test_ping "google.com"
 test_ping "172.18.0.254"
@@ -31,13 +38,12 @@ test_ping "172.28.255.254"
 test_ping "172.29.255.254"
 test_ping "172.30.255.254"
 
-echo "Fin d'execution du script $(date)" >> test_internet_v2.log
+echo "Fin du script $(date)" >> test_internet_v2.log
 
-echo "Afficher les logs? (y/n)"
+echo "Afficher les logs ? (o/n)"
 
-read ans
+read reponse
 
-if [ "$ans" = "y" ];then 
+if [ "$reponse" = "o" ];then 
   cat test_internet_v2.log
 fi
- 

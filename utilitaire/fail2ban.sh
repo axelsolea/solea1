f3d7c0919e1bd70514f4527eb897a16c4c6e2da5
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-04-11 09:50:07 +0200

    Create fail2ban.sh

---
+++ utilitaire/fail2ban.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+#Author : Axel 
+#Date : 15/02/2024
+#Last edit : 16/02/2024
+#Desc : make an ip get banned via ssh on asterisk
+ip=192.168.141.252
+#ip=172.18.0.4
+
+###################################################################
+# Ce script teste que le mécanisme de fail2ban fonctionne bien en
+# effectuant plusieurs tentatives de connexion jusqu'à être "banni".
+###################################################################
+
+echo "Lancement du script..."
+for i in 1 2 3 4
+do
+	echo "Tentative $i" 
+	sshpass -p motdepasseerrone ssh admin-solea@${ip}
+done
+echo "Script terminé"

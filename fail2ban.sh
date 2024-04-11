cf7243e121ccf5d07e719e74a8052de914627cb9
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-04-11 08:19:11 +0200

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -6,6 +6,11 @@
 ip=192.168.141.252
 #ip=172.18.0.4
 
+###################################################################
+# Ce script test que le mécanisme de fail2ban fonctionne bien avec
+# plusieurs de tentaive de connexion jusqu'a etre "bannie"
+###################################################################
+
 echo "Lancement du script..."
 for i in 1 2 3 4
 do

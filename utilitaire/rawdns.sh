1a97a974bd12156453a025fc65d039dcfa9d6d33
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-04-11 09:45:32 +0200

    Create rawdns.sh

---
+++ utilitaire/rawdns.sh
@@ -0,0 +1,40 @@
+#!/bin/env bash
+#Author : Axel 
+#Date : 16/02/2024
+#Last edit : 16/02/2024
+#Desc : Raw dns testing
+
+####################################################################
+# ce script test une résolution de nom de chaque serveur du site
+# de l'entreprise SOLEA
+####################################################################
+
+echo "Script started"
+
+echo "Resolving resolver1"
+nslookup resolver1.solea.local
+nslookup 172.18.0.200
+
+echo "Resolving zone1"
+nslookup zone1.solea.local
+nslookup 172.18.0.202
+
+echo "Resolving dhcp"
+nslookup dhcp.solea.local
+nslookup 172.18.0.253
+
+echo "Resolving toip"
+nslookup toip.solea.local
+nslookup 172.18.0.4
+
+echo "Resolving share (samba)"
+nslookup share.solea.local
+nslookup 172.18.0.252
+
+echo "Resolving gw"
+nslookup gw.solea.local
+nslookup 172.18.0.254
+
+echo "Resolving esxi"
+nslookup esxi.solea.local
+#nslookup 192.168.141.3 => entrée ptr inexistante

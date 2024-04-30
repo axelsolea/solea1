d1c3e68258e2c17101d7b45db90e71d5f2d05fe6
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-04-30 17:07:02 +0200

    Create main.py

---
+++ samba/main.py
@@ -0,0 +1,11 @@
+from extractname import ExtractName
+
+
+def main() -> None:
+    extractname: ExtractName = ExtractName("list_users.csv")
+    data: pd.DataFrame = extractname.get_users_list()
+    extractname.show_data()
+
+
+if __name__ == "__main__":
+    main()

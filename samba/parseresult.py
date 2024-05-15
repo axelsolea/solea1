920b04147f884e4dc7551fc70b99f6310addaa22
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-05-15 08:41:44 +0200

    Update parseresult.py

--- samba/parseresult.py
+++ samba/parseresult.py
@@ -92,9 +92,9 @@ class ParseResult:
         
         # Écrire des détailles de chaque test
         row += 6  # Aller à la prochaine ligne pour le prochain ensemble de données
-        worksheet.write(row, 0, "numéro de test", gray_background_format)
-        worksheet.write(row, 1, "description", gray_background_format)
-        worksheet.write(row, 2, "statut", gray_background_format)
+        worksheet.write(row, 0, "Numéro de test", gray_background_format)
+        worksheet.write(row, 1, "Description", gray_background_format)
+        worksheet.write(row, 2, "Statut", gray_background_format)
 
         j: int = 1
         row += 1

07641264966e6270cd2fdd425111f579af7db9bd
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-05-22 08:25:12 +0200

    Update testshare.py

--- samba/testshare.py
+++ samba/testshare.py
@@ -167,18 +167,18 @@ class TestShare:
             self._data.append({"desc": f"Montage du dossier {self._mount_point} au partage de fichier", "status": "NOK"})
 
         # Création d'un fichier utilisateur sur le partage
-        if self.create_file(f"{self._user}.txt", f"contenu de {self._user}"):
+        if self.create_file(f"{self._user}.txt", f"contenue de {self._user}"):
             self._cpt += 1
             self._data.append({"desc": f"Création d'un fichier text {self._user}.txt", "status": "OK"})
         else:
             self._data.append({"desc": f"Création d'un fichier text {self._user}.txt", "status": "NOK"})
 
         # Lecture du fichier utilisateur sur le partage
-        if self.read_file(f"{self._user}.txt", f"contenu de {self._user}"):
+        if self.read_file(f"{self._user}.txt", f"contenue de {self._user}"):
             self._cpt += 1
-            self._data.append({"desc": f"Lecture d'un fichier text {self._user}.txt avec le contenu: 'contenu de {self._user}'", "status": "OK"})
+            self._data.append({"desc": f"Lecture d'un fichier text {self._user}.txt avec le contenu: 'contenue de {self._user}'", "status": "OK"})
         else:
-            self._data.append({"desc": f"Lecture d'un fichier text {self._user}.txt avec le contenu: 'contenu de {self._user}'", "status": "NOK"})
+            self._data.append({"desc": f"Lecture d'un fichier text {self._user}.txt avec le contenu: 'contenue de {self._user}'", "status": "NOK"})
 
         # Suppression du point de montage
         if self.umount():
